35ae46c62550f0e1202247beacdfcd8fad28d0db
axxving <axxving@gmail.com>
2025-02-01 03:22:36 -0600

    chore: start server listening

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 // Importar dependencias
 const { connection } = require('./database/connection_database');
+const express = require('express');
+const cors = require('cors');
 
 // conexion a la base de datos
 connection();
 
 // Servidor con node
-console.log('Servidor node iniciado');
+const app = express();
+const puerto = 3900;
 
 // Configurar cors
+app.use(cors());
 
 // Convertir datos del body a objetos js
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Cargar archivo de rutas
+// Ruta prueba
+app.get('/ruta-prueba', (req, res) => {
+    return res.status(200).json({
+        mensaje: 'Ruta funcionando correctamente',
+    });
+});
 
 // Poner el servidor a escuchar peticiones http
+app.listen(puerto, () => {
+    console.log('Servidor node iniciado en puerto:', puerto);
+});
